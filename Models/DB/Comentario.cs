7f66d5562bd977fac4f9246f472947df40a133d9
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-07-05 17:42:43 -0500

    Agregar archivos de proyecto.

---
+++ Models/DB/Comentario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebCineflex.Models.DB;
+
+public partial class Comentario
+{
+    public int IdComentario { get; set; }
+
+    public int IdResenaF { get; set; }
+
+    public int IdUserF { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Cuerpo { get; set; } = null!;
+
+    public string RutaArchivo { get; set; } = null!;
+
+    public DateTime FechaComentario { get; set; }
+
+    public virtual Resena IdResenaFNavigation { get; set; } = null!;
+
+    public virtual Usuario IdUserFNavigation { get; set; } = null!;
+}
